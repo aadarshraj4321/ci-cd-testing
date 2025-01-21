f60c8e8c2dd2ff12257bd6e3649fc3129299d5f9
aadarshraj4321 <aadarshraj4321@gmail.com>
2025-01-21 20:45:09 +0530

    adding cd to docker to ec2

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    // Ensuring the app binds to all interfaces for external access
+    webpack(config, { isServer }) {
+      if (isServer) {
+        // Ensure Next.js listens on all network interfaces (0.0.0.0)
+        config.devServer.host = '0.0.0.0';
+      }
+      return config;
+    }
+  };
+  
+  export default nextConfig;
+  

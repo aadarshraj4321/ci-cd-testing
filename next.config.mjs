377a05c23870111a6f4bee68505950b4c5579d84
aadarshraj4321 <aadarshraj4321@gmail.com>
2025-01-21 20:50:37 +0530

    adding cd to docker to ec2

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack(config, { isServer, dev }) {
-      if (dev && isServer) {
-        // This ensures that Next.js listens on 0.0.0.0 only during development
-        config.devServer.host = '0.0.0.0';
-      }
-      return config;
-    }
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
